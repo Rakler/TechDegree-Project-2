69adf3b660bd7329d5051a508055976b2bde94f7
Gert Poulsen <pojonobi1@hotmail.com>
2019-06-26 18:48:39 +0100

    Added comments, and initial search functionality

--- js/script.js
+++ js/script.js
@@ -21,25 +21,11 @@ var page = 1;
 const numberPerPage = 10;
 const mainDiv = document.querySelector('.page');
 var numberOfPages = Math.ceil(studentList.length / numberPerPage);
-const ul = document.createElement('ul');
-const paginationDiv = document.createElement('div');
 
-//for testing purposes
-//console.log(list);
 
 /***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
+  The showPage() function deals with the rendering of the view, and the list of the students. The students are shown or hidden based on what page you are on.
+  The list that is meant to be rendered is passed in as a parameter alongside what page you are on.
 ***/
 const showPage = (list, page) => {
   const startIndex =  (numberPerPage * page) - numberPerPage;
@@ -54,43 +40,83 @@ const showPage = (list, page) => {
   }
 }
 
+//Call the showPage() function to generate the initial view.
 showPage(studentList, page);
+
 /***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
+   The appendPageLinks function takes care of creating all of the elements needed to apply pagination to the side.
+   It first create the div itself, then loops through the number of pages and for each page, it generates a ul with anchor tags nested inside of li for each page.
 ***/
 const appendPageLinks = (list) =>{
-	
+  const paginationDiv = document.createElement('div');
 	paginationDiv.className = 'pagination';
-	
-	
+  const ul = document.createElement('ul');
+
 	for(let i = 1; i < numberOfPages + 1; i++){
 		const li = document.createElement('li');
 		const a = document.createElement('a');
 		a.textContent = i;
 		li.appendChild(a);
 		ul.appendChild(li);
-		
+
 	}
 	paginationDiv.appendChild(ul);
 	ul.childNodes[0].childNodes[0].className = 'active';
 	mainDiv.appendChild(paginationDiv);
-	
+
 }
+
+//Call appendPageLinks function to generate the initial links
+appendPageLinks(studentList);
 
 mainDiv.addEventListener('click', (e) => {
 	const tags = document.getElementsByTagName('a');
 	if(e.target.tagName === 'A'){
 		for(let i = 0; i < numberOfPages; i++){
 			tags[i].className = '';
+      tags[i].href = '#';
 		}
 		e.target.className = 'active';
 		showPage(studentList, parseInt(e.target.textContent));
-		//console.log(parseInt(e.target.textContent));
 	}
 });
 
-appendPageLinks(studentList);
-const getNumberOfPages = () =>{ Math.ceil(list.length / numberPerPage); }
+//const getNumberOfPages = (list) =>{
+  //return Math.ceil(list.length / numberPerPage);
+//}
+
+/***
+  Selecting the div with the class "page-header" in order to generate the markup
+  for the search functionality
+***/
+const headDiv= document.querySelector('.page-header');
+const studentSearchDiv = document.createElement('div');
+studentSearchDiv.className = 'student-search';
+const searchInput = document.createElement('input');
+searchInput.placeholder = 'Search for students...';
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+studentSearchDiv.appendChild(searchInput);
+studentSearchDiv.appendChild(searchButton);
+headDiv.appendChild(studentSearchDiv);
+
+/***
+  EventListener that is supposed to check the input field for the value, and then
+  filter the student list.
+***/
+headDiv.addEventListener('keyup',(e)=>{
+  if(e.target.tagName === 'BUTTON'){
+    const input = document.getElementsByTagName('input')[0];
+    var filteredList;
+    for(let i = 0; i < studentList.length; i++){
+      if(input.value.includes(studentList[i].children[0].children[1].textContent)){
+        filteredList += [studentList[i]];
+        console.log(filteredList.length);
+      } else{
+      }
+    }
+    }
+  }
+);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
